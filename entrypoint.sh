a21228c3534bea3fc4367e56a70e031144b224d1
Iresh Dissanayaka <ireshmmi@gmail.com>
2024-08-23 08:49:05 +0000

    change default input format to yaml

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 TEMPLATE_FILE="${HOME}/template.j2"
 INPUT_FILE="${HOME}/input"
 OUTPUT_FILE="${HOME}/output"
-FORMAT="${FORMAT:-json}"
+FORMAT="${FORMAT:-yaml}"
 
 echo "Input file: ${INPUT_FILE}"
 echo "Template file: ${TEMPLATE_FILE}"
 echo "Output file: ${OUTPUT_FILE}"
 
-$HOME/.local/bin/jinja2 --format json "${TEMPLATE_FILE}" "${INPUT_FILE}" > "${OUTPUT_FILE}"
+$HOME/.local/bin/jinja2 --format "$FORMAT" "${TEMPLATE_FILE}" "${INPUT_FILE}" > "${OUTPUT_FILE}"
